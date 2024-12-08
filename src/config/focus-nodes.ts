7d8f5f231e4f42a429c325cab35b16ac1139ad00
Bereket <bereketsiyum@gmail.com>
2024-12-08 11:06:55 +0900

    restrict which nodes to apply focus extension

---
+++ src/config/focus-nodes.ts
@@ -0,0 +1,10 @@
+// Define which nodes should receive focus styling
+export const FOCUS_ALLOWED_NODES = [
+  "image",
+  "youtube",
+  "video",
+  "codeBlock",
+  "table",
+  "sandbox",
+  "live-code-block",
+];

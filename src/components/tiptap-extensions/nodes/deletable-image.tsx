8be70ecf9a8789488079a0e8a58666545b03709d
Bereket <bereketsiyum@gmail.com>
2025-01-08 12:05:32 +0700

    add a placeholder blur image when image is loading in the editor

--- src/components/tiptap-extensions/nodes/deletable-image.tsx
+++ src/components/tiptap-extensions/nodes/deletable-image.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { NodeViewProps, NodeViewWrapper } from "@tiptap/react";
 
 import { DeletableNodeWrapper } from "../deletable-node-wrapper";
 import { cn } from "@/lib/utils";
+import { dynamicBlurDataUrl } from "@/lib/image/utils";
 
 export function DeletableImage({ node, deleteNode }: NodeViewProps) {
-  console.log(typeof node);
   const { width, height, alt, src, class: className } = node.attrs;
+  const [blurDataUrl, setBlurDataUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchBlurDataUrl = async () => {
+      const url = await dynamicBlurDataUrl(src as string);
+      setBlurDataUrl(url);
+    };
+    fetchBlurDataUrl();
+  }, [src]);
+
   return (
     <NodeViewWrapper>
       <DeletableNodeWrapper
@@ -16,6 +27,8 @@ export function DeletableImage({ node, deleteNode }: NodeViewProps) {
       >
         <div className="relative mx-auto my-4" style={{ width, height }}>
           <Image
+            blurDataURL={blurDataUrl || undefined}
+            placeholder={blurDataUrl ? "blur" : undefined}
             className={cn("rounded-md border", className)}
             alt={alt || ""}
             src={src as string}

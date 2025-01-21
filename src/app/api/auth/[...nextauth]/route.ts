5f51884892009e6a2a815e5314dfdc3677284301
Bereket <bereketsiyum@gmail.com>
2025-01-21 11:29:16 +0900

    Add user avatars and profile images; update authentication flow and user navigation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
-import NextAuth, { type AuthOptions } from "next-auth";
+import NextAuth, { User, type AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { authApi } from "@/lib/api";
-import type { User, LoginResponse } from "@/types/api";
+import { signin } from "@/lib/actions/user";
+import { LoginResponse } from "@/types/api";
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -24,17 +25,27 @@ const authOptions: AuthOptions = {
             return null;
           }
 
-          const response = (await authApi.login({
+          const response = (await signin({
             email: credentials.email,
             password: credentials.password,
           })) as LoginResponse;
 
           if (response.access_token) {
             // Get user profile using the access token
-            const userProfile = (await authApi.getUserProfile(
-              response.access_token
-            )) as User;
+            // const userProfile = (await authApi.getUserProfile(
+            //   response.access_token
+            // )) as User;
+            const userProfileResponse = await fetch(
+              `${process.env.NEXT_PUBLIC_API_URL}/users/me`,
+              {
+                headers: {
+                  Authorization: `Bearer ${response.access_token}`,
+                },
+              }
+            );
 
+            const userProfile = (await userProfileResponse.json()) as User;
+            console.log("userProfile", userProfile);
             return {
               ...userProfile,
               accessToken: response.access_token,
@@ -52,7 +63,7 @@ const authOptions: AuthOptions = {
   callbacks: {
     async signIn({ user }) {
       // Only allow active superusers to sign in
-      return user.is_active && user.is_superuser;
+      return user.is_active;
     },
     async jwt({ token, user }) {
       if (user) {
@@ -61,6 +72,9 @@ const authOptions: AuthOptions = {
         token.is_superuser = user.is_superuser;
         token.is_active = user.is_active;
         token.full_name = user.full_name;
+        token.email_verified = user.email_verified;
+        token.id = user.id;
+        token.image = user.is_superuser ? "/profile.jpg" : "/avatar.png";
       }
       return token;
     },
@@ -70,7 +84,12 @@ const authOptions: AuthOptions = {
         session.user.is_superuser = token.is_superuser;
         session.user.is_active = token.is_active;
         session.user.full_name = token.full_name;
+        session.user.email_verified = token.email_verified;
         session.accessToken = token.accessToken;
+        session.user.id = token.id;
+        session.user.image = token.is_superuser
+          ? "/profile.jpg"
+          : "/avatar.png";
       }
       return session;
     },
--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -12,30 +12,34 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { breakpoints, useMediaQuery } from "@/lib/hooks/use-media-query";
-import { Button } from "./ui/button";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { TooltipProvider } from "./ui/tooltip";
+import { cn } from "@/lib/utils";
+
+export function NavUser() {
   const isMobile = !useMediaQuery(breakpoints.md);
+  const { data: session } = useSession();
+  const full_name = session?.user.full_name || "";
+  const avatar = session?.user.image || "/avatar.png";
+  const email = session?.user.email || "";
+  const is_superuser = session?.user.is_superuser || false;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="rounded-lg max-w-fit gap-1">
-          <Avatar className="h-8 w-8 rounded-full">
-            <AvatarImage src={user.avatar} alt={user.name} />
-            <AvatarFallback className="rounded-lg">Bake</AvatarFallback>
+        <div className="flex gap-1 items-center justify-center">
+          <Avatar className="h-12 w-12 rounded-full hover:cursor-pointer hover:outline hover:outline-muted-foreground">
+            <AvatarImage
+              src={avatar}
+              alt={full_name}
+              className={cn("object-cover", is_superuser && "object-top")}
+            />
+            <AvatarFallback className="rounded-lg">{full_name}</AvatarFallback>
           </Avatar>
-          <Icons.chevronDown className="ml-auto size-4" />
-        </Button>
+          <Icons.chevronsUpDown className="ml-auto size-4" />
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent
         className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
@@ -46,15 +50,34 @@ export function NavUser({
         <DropdownMenuLabel className="p-0 font-normal">
           <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
             <Avatar className="h-8 w-8 rounded-lg">
-              <AvatarImage src={user.avatar} alt={user.name} />
+              <AvatarImage
+                src={avatar}
+                alt={full_name}
+                className={cn(
+                  "object-cover object-top",
+                  is_superuser && "object-top"
+                )}
+              />
               <AvatarFallback className="rounded-lg">User</AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-semibold">{user.name}</span>
-              <span className="truncate text-xs">{user.email}</span>
+              <span className="truncate font-semibold">{full_name}</span>
+              <span className="truncate text-xs">{email}</span>
             </div>
           </div>
         </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="flex items-center gap-2 cursor-not-allowed"
+          onClick={() => {}}
+        >
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>Choose Avatar</TooltipTrigger>
+              <TooltipContent>Coming Soon</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
           className="flex items-center gap-2"
